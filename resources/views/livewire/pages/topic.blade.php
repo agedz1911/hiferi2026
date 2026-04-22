1e813c3a723c33b755cfb64fd235aba5bb30ae54
AV Pro <asep.cgeds@gmail.com>
2026-04-22 14:59:17 +0700

    Refactor topic list to include date-specific subtopics

--- resources/views/livewire/pages/topic.blade.php
+++ resources/views/livewire/pages/topic.blade.php
@@ -17,22 +17,27 @@
             ->whereNotNull('name')
             ->unique('name')
             ->values();
-            $specialSubs = $categoryTopics
-            ->where('name', 'Thursday, October 8, 2026')
-            ->pluck('sub_name')
-            ->filter() // buang null/empty
-            ->unique() // hindari duplikasi
-            ->values();
             @endphp
 
             <ol class="list-decimal list-inside space-y-2">
                 @foreach ($mainTopics as $main)
                 <li class="text-gray-800">
-                    @if ($main->name === 'Thursday, October 8, 2026')
-                    Thursday, October 8, 2026
-                    @if ($specialSubs->isNotEmpty())
+                    @if (in_array($main->name, ['Thursday, October 8, 2026', 'Friday, October 9, 2026']))
+                    {{ $main->name }}
+
+                    @php
+                    // Ambil sub_name KHUSUS untuk tanggal ini saja
+                    $dateSpecificSubs = $categoryTopics
+                    ->where('name', $main->name) // Filter berdasarkan tanggal spesifik
+                    ->pluck('sub_name')
+                    ->filter() // buang null/empty
+                    ->unique() // hindari duplikasi
+                    ->values();
+                    @endphp
+
+                    @if ($dateSpecificSubs->isNotEmpty())
                     <ul class="list-disc list-inside ml-6 mt-1 space-y-1">
-                        @foreach ($specialSubs as $sub)
+                        @foreach ($dateSpecificSubs as $sub)
                         <li>{{ $sub }}</li>
                         @endforeach
                     </ul>
