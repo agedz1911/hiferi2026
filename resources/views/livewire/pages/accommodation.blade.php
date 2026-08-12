4a1cda389a21b155791d685ff09b9995bc85df05
AV Pro <asep.cgeds@gmail.com>
2026-08-12 16:22:24 +0700

    Refactor accommodation section layout and improve button styles

--- resources/views/livewire/pages/accommodation.blade.php
+++ resources/views/livewire/pages/accommodation.blade.php
@@ -8,12 +8,14 @@
 
     <section class="px-5 md:px-10 pt-0 pb-10 md:py-20">
         <div class="text-center md:text-start mb-5">
-            <h2 class="mb-3 uppercase text-4xl tracking-wide font-bold">Hotel <span class="text-[#39B54A]">Reservation</span></h2>
-            <p class="text-gray-500">The organizers of the ... congress have secured competitive rates at a variety of
+            <h2 class="mb-3 uppercase text-4xl tracking-wide font-bold">Hotel <span
+                    class="text-[#39B54A]">Reservation</span></h2>
+            {{-- <p class="text-gray-500">The organizers of the ... congress have secured competitive rates at a variety
+                of
                 hotels near the Venue to accommodate delegates
                 with different budgets and preferences. Hotel reservations will open and are subject
                 to availability. It is advisable to book your preferred hotel as soon as possible
-            </p>
+            </p> --}}
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-center">
@@ -26,8 +28,7 @@
                             class="absolute top-1 right-4 translate-y-5 bg-[#39B54A] rounded-xl text-white px-3 py-1">
                             <p class="text-xs">' . $accommodation->tag . '</p>
                         </span>' : " " !!}
-                        <a href="javascript:void(0)"><img
-                                src="{{ asset('storage/' . $accommodation->image) }}"
+                        <a href="javascript:void(0)"><img src="{{ asset('storage/' . $accommodation->image) }}"
                                 alt="{{$accommodation->hotel_name}}" class="w-full object-cover rounded-xl"></a>
                     </div>
                     <div class="mb-3 px-3">
@@ -40,27 +41,27 @@
                                 @endif
                                 @endfor
                         </div>
-                        <p class="mt-2 mb-5 text-xs"><i
-                                class="fa-solid fa-circle-info"></i>
+                        <p class="mt-2 mb-5 text-xs"><i class="fa-solid fa-circle-info"></i>
                             {{$accommodation->distance}}
                         </p>
                         <p class="text-sm"> Estimated Cost/Night</p>
                         <div class="text-sm mb-6 flex justify-center gap-3">
-                            <p class="text-[#39B54A] font-semibold mb-0"><span
-                                    class="fw-normal">IDR</span>
+                            <p class="text-[#39B54A] font-semibold mb-0"><span class="fw-normal">IDR</span>
                                 {{number_format($accommodation->idr_price, 0, ',', '.')}}
                             </p>
-                            <p class="text-[#39B54A] font-semibold"><span
-                                    class="fw-normal">USD</span>
+                            @if ($accommodation->usd_price !== null)
+                            <p class="text-[#39B54A] font-semibold"><span class="fw-normal">USD</span>
                                 {{$accommodation->usd_price}}
                             </p>
+                            @endif
                         </div>
                     </div>
                     <div class="grid grid-cols-2 gap-2 px-2">
                         <a href="{{$accommodation->url ? $accommodation->url : 'javascript:void(0)'}}"
-                            class="btn bg-[#39B54A] hover:bg-[#b71b69] text-white w-full rounded-lg">Book Now</a>
+                            class="btn btn-success w-full rounded-lg"><i class="fa fa-hotel"></i> Book Now</a>
                         <a href="{{$accommodation->direction ? $accommodation->direction : 'javascript:void(0)'}}"
-                            class="btn bg-[#39B54A] hover:bg-[#b71b69] text-white w-full btn-outline rounded-lg"><i class="fa-solid fa-location-dot mx-1"></i>Direction</a>
+                            class="btn btn-success w-full btn-outline rounded-lg"><i
+                                class="fa-solid fa-location-dot mx-1"></i>Direction</a>
                     </div>
                 </div>
             </div>
